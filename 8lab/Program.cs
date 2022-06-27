beb8d90370e5cb4480bb195a1c5d52b3e7574a76
Евгений Ташогло <tashoglo00@mail.ru>
2022-06-27 17:32:22 +0200

    Добавьте файлы проекта.

---
+++ 8lab/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("Введите первое дробное:");
+    double first = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите второе дробное:");
+    double second = double.Parse(Console.ReadLine());
+    Console.WriteLine("Ответ " + Math.Pow(first, second));
+    while (true) ;
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Ошибка: {e.Message}");
+}
